2e3de96e8517ad38039743bb67024b061fed4df1
Kristýna Kopalová <kopalova30@gmail.com>
2023-10-22 17:02:51 +0200

    ukol 5

--- hra.js
+++ hra.js
@@ -1,5 +1,7 @@
 import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
+let winner = null;
+
 let isCurrentTurnCircle = true;
 const currentPlayerImageCircle = document.createElement('img');
 currentPlayerImageCircle.setAttribute('src', 'circle.svg');
@@ -13,7 +15,7 @@ const playingAreaElement = document.querySelector('.playing_area');
 const currentPlayerElement = document.querySelector('.current_player');
 currentPlayerElement.appendChild(currentPlayerImageCircle);
 
-const checkWinner = () => {
+const createInputBoard = () => {
   const allItems = document.querySelectorAll('.playing_area_item');
   const findWinnerInput = [];
   allItems.forEach((item) => {
@@ -25,7 +27,10 @@ const checkWinner = () => {
       findWinnerInput.push('_');
     }
   });
-  const winner = findWinner(findWinnerInput);
+  return findWinnerInput;
+};
+
+const showAlert = () => {
   if (winner === 'o') {
     alert('Vyhrálo kolečko. Gratuluji!');
     location.reload();
@@ -38,25 +43,60 @@ const checkWinner = () => {
   }
 };
 
+const checkWinner = () => {
+  const findWinnerInput = createInputBoard();
+  const winner = findWinner(findWinnerInput);
+  return winner;
+};
+
 for (let i = 0; i < 100; i++) {
   const button = document.createElement('button');
   button.classList.add('playing_area_item');
-  button.addEventListener('click', (event) => {
+  button.addEventListener('click', async (event) => {
+    event.target.disabled = true;
+
     if (isCurrentTurnCircle) {
       button.classList.add('playing_area_item--circle');
       isCurrentTurnCircle = false;
       currentPlayerElement.removeChild(currentPlayerImageCircle);
 
       currentPlayerElement.appendChild(currentPlayerImageCross);
+
+      winner = checkWinner();
+      if (winner != null) {
+        setTimeout(showAlert, 300);
+        return;
+      }
+
+      const response = await fetch(
+        'https://piskvorky.czechitas-podklady.cz/api/suggest-next-move',
+        {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json',
+          },
+          body: JSON.stringify({
+            board: createInputBoard(),
+            player: 'x',
+          }),
+        },
+      );
+      const data = await response.json();
+      const allItems = document.querySelectorAll('.playing_area_item');
+      const index = data.position.x + data.position.y * 10;
+      allItems[index].click();
     } else {
       button.classList.add('playing_area_item--cross');
       isCurrentTurnCircle = true;
       currentPlayerElement.removeChild(currentPlayerImageCross);
 
       currentPlayerElement.appendChild(currentPlayerImageCircle);
+      winner = checkWinner();
+      if (winner != null) {
+        setTimeout(showAlert, 300);
+        return;
+      }
     }
-    event.target.disabled = true;
-    setTimeout(checkWinner, 300);
   });
 
   playingAreaElement.appendChild(button);
